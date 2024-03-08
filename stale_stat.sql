96b6731fc739531b312bbd68681f9c16ad1cef85
oraqba <128472531+oraqba@users.noreply.github.com>
2024-03-08 14:29:36 +0100

    Create stale_stat.sql

---
+++ stale_stat.sql
@@ -0,0 +1,80 @@
+accept owner -
+       prompt 'Enter owner name : ' -
+       default ''
+
+accept table_name -
+       prompt 'Enter table name : ' -
+       default ''
+
+accept index_name -
+       prompt 'Enter index name : ' -
+       default ''
+
+accept ORACLE_MAINTAINED -
+       prompt 'Include ORACLE_MAINTAINED (Y/[N]) : ' -
+       default 'N'
+
+set feedback off
+--set sqlblanklines on
+set verify off
+set pages 300 lines 300
+col owner for a30
+col table_owner for a30
+col index_name for a30
+col table_name for a30
+col partition_name for a30
+col inserts for 9999999999
+col updates for 9999999999
+col deletes for 9999999999
+col COLUMN_NAME for a30
+
+prompt 'Table modification'
+
+SELECT TABLES.OWNER, TABLES.TABLE_NAME,
+ROUND((DELETES + UPDATES + INSERTS)/NUM_ROWS*100) PERCENTAGE,
+DELETES,UPDATES,INSERTS,NUM_ROWS
+FROM DBA_TABLES TABLES, DBA_TAB_MODIFICATIONS MODIFICATIONS
+WHERE TABLES.OWNER = MODIFICATIONS.TABLE_OWNER
+AND TABLES.TABLE_NAME = MODIFICATIONS.TABLE_NAME
+AND NUM_ROWS > 0
+AND ROUND ( (DELETES + UPDATES + INSERTS) / NUM_ROWS * 100) >= 10
+AND TABLES.OWNER like '&&owner%'
+AND TABLES.TABLE_NAME like '&&table_name%'
+AND TABLES.OWNER in (select username from dba_users where ORACLE_MAINTAINED in ('N','&&ORACLE_MAINTAINED'))
+ORDER BY 1,3 desc
+/
+
+
+prompt 'Table stale stats'
+
+select owner,table_name,partition_name,num_rows,last_analyzed,stale_stats
+from dba_tab_statistics
+where stale_stats='YES'
+and OWNER like '&&owner%'
+AND TABLE_NAME like '&&table_name%'
+AND OWNER in (select username from dba_users where ORACLE_MAINTAINED in ('N','&&ORACLE_MAINTAINED'))
+order by owner,table_name
+/
+
+prompt 'Index stale stats'
+
+select table_owner,table_name,index_name,partition_name,num_rows,last_analyzed,stale_stats
+from dba_ind_statistics
+where stale_stats='YES'
+and TABLE_OWNER like '&&owner%'
+AND TABLE_NAME like '&&table_name%'
+AND INDEX_NAME like '&&index_name%'
+AND TABLE_OWNER in (select username from dba_users where ORACLE_MAINTAINED in ('N','&&ORACLE_MAINTAINED'))
+order by owner,table_name,index_name
+/
+
+col gather_stats for a100
+select distinct 'exec DBMS_STATS.GATHER_SCHEMA_STATS('''||owner||''',options =>''GATHER AUTO'',degree=>4);' GATHER_STATS from dba_tab_statistics
+where stale_stats='YES' 
+AND OWNER in (select username from dba_users where ORACLE_MAINTAINED in ('N','&&ORACLE_MAINTAINED'))
+order by 1;
+
+undef owner
+undef table_name
+undef index_name
+undef ORACLE_MAINTAINED
