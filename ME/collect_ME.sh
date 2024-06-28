dff5d705202f65886ab3571382cf717f1c9d39f5
oraqba <128472531+oraqba@users.noreply.github.com>
2024-06-28 11:57:13 +0200

    Update collect_ME.sh

--- ME/collect_ME.sh
+++ ME/collect_ME.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-
 export ORACLE_SID=emrep
 export ORAENV_ASK=NO;. oraenv > /dev/null;
 export SPOOLFILE=/tmp/emcli_collect_ME_tmp.sql
-$ORACLE_HOME/bin/sqlplus -s sysman/<SYSMANPASS> <<EOF
+export SQL_CONN='/ as sysdba'
+$ORACLE_HOME/bin/sqlplus -s ${SQL_CONN} <<EOF
 set lines 300 pages 0
 set feed 0
 set head off
 set echo off
+alter session set current_schema=SYSMAN;
 spool ${SPOOLFILE}
 select 'collect_metric  -target_type=oracle_database  -target_name='||target_name
 ||' -metric_name="ME\$init_param_temp_file_drift_collector"'
