a49853e78c1d8a21a83811a3477a66bb1ed83516
oraqba <128472531+oraqba@users.noreply.github.com>
2024-11-01 15:47:50 +0100

    Create purge_sqlid.sql

---
+++ purge_sqlid.sql
@@ -0,0 +1,21 @@
+set serveroutput on  
+set pagesize 9999  
+set linesize 155  
+var name varchar2(50)
+
+prompt WARNING SQL_ID WILL BE PURGED FROM THE SHARED POOL  
+accept sql_id prompt 'Enter value for sql_id: '
+
+BEGIN
+
+select address||','||hash_value into :name  
+from v$sqlarea  
+where sql_id like '&&sql_id';
+
+dbms_shared_pool.purge(:name,'C',1);
+
+END;  
+/
+
+undef sql_id  
+undef name 
