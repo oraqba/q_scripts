c388627ad1f478434a5a3d1fad4754b34963222f
oraqba <128472531+oraqba@users.noreply.github.com>
2025-08-21 10:10:39 +0200

    Update rman_speed.sql

--- rman_speed.sql
+++ rman_speed.sql
@@ -30,3 +30,18 @@ AND
    opname NOT LIKE '%aggregate%'
 AND   opname like 'RMAN%'
 order by pct_done;
+
+
+SELECT
+     a.device_type, a.TYPE, b.status, b.mbytes_processed AS "Total MB", a.filename, ROUND(a.bytes/(1024*1024)) AS "Size MB", TO_CHAR(a.open_time,'dd-mon-yyyy hh24:mi:ss') AS open_time,
+     ROUND(a.elapsed_time/100) AS "Time (s)", ROUND(a.elapsed_time/100) AS "Elapsed (s)", ROUND(a.effective_bytes_per_second/(1024*1024)) AS "MB/s"
+     FROM v$backup_async_io a, v$rman_status b
+     WHERE a.rman_status_recid = b.recid
+     ORDER BY a.use_count;
+
+SELECT
+     a.device_type, a.TYPE, b.status, b.mbytes_processed AS "Total MB", a.filename, ROUND(a.bytes/(1024*1024)) AS "Size MB", TO_CHAR(a.open_time,'dd-mon-yyyy hh24:mi:ss') AS open_time,
+     ROUND(a.elapsed_time/100) AS "Time (s)", ROUND(a.elapsed_time/100) AS "Elsapsed (s)", ROUND(a.effective_bytes_per_second/(1024*1024)) AS "MB/s"
+     FROM v$backup_sync_io a, v$rman_status b
+     WHERE a.rman_status_recid = b.recid
+     ORDER BY a.use_count;
